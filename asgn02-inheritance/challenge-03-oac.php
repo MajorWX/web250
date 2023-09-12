fca8b1d0a6708fb1c685e104d4446f52f3b34113
Kira Beitler <majorwx2@gmail.com>
2023-09-12 17:22:43 -0400

    Completed the object access control challenge

--- asgn02-inheritance/challenge-03-oac.php
+++ asgn02-inheritance/challenge-03-oac.php
@@ -1,14 +1,65 @@
 <?php
 
-class Food {
+class Bicycle {
 
-  var $name;
-  var $isEdible = true;
-  var $calories;
-  var $color;
-  var $canSpoil;
-  var $lifeTimeDays = 30;
-  var $age = 0;
+  public $brand;
+  public $model;
+  public $year;
+  public $description = 'Used bicycle';
+  private $weight_kg = 0.0;
+  protected $wheels = 2;
 
+  public function name(){
+    return $this->brand . " " . $this->model . " (" . $this->year . ")";
+  }
+
+  public function weight_lbs() {
+    return floatval($this->weight_kg) * 2.2046226218 . " lbs";
+  }
+
+  public function set_weight_lbs($value) {
+    $this->weight_kg = floatval($value) / 2.2046226218;
+  }
+
+  public function wheel_details() {
+    return "It has " . $this->wheels . " wheels.";
+  }
+
+  public function set_weight_kg($value) {
+    $this->weight_kg = floatval($value);
+  }
+
+  public function get_weight_kg() {
+    return $this->weight_kg . " kg";
+  }
+}
+
+class Unicycle extends Bicycle {
+
+  public $description = 'Used unicycle';
+  protected $wheels = 1;
 
 }
+
+$trek = new Bicycle;
+$trek->brand = 'Trek';
+$trek->model = 'Emonda';
+$trek->year = '2017';
+
+$uni = new Unicycle;
+
+echo "Bicycle: " . $trek->wheel_details() . "<br />";
+echo "Unicycle: " . $uni->wheel_details() . "<br />";
+echo "<hr />";
+
+echo "Set weight using kg<br />";
+$trek->set_weight_kg(1);
+echo $trek->weight_kg() . "<br />";
+echo $trek->weight_lbs() . "<br />";
+echo "<hr />";
+
+echo "Set weight using lbs<br />";
+$trek->set_weight_lbs(2);
+echo $trek->weight_kg() . "<br />";
+echo $trek->weight_lbs() . "<br />";
+echo "<hr />";
