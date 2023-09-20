fc5f26f9d8a371ab87ac4a708bbad46431d4011f
Kira Beitler <majorwx2@gmail.com>
2023-09-20 01:29:32 -0400

    Finished modifying Bird.php

---
+++ static-challenge/Bird.php
@@ -0,0 +1,36 @@
+<?php
+
+use Bird as GlobalBird;
+
+class Bird {
+    var $habitat;
+    var $food;
+    var $nesting = "tree";
+    var $conservation;
+    var $song = "chirp";
+    var $flying = "yes";
+    static $instance_count = 0;
+    static $egg_num = 0;
+
+    function can_fly() {
+        return ($this->flying == "yes" ) ? "can fly" : "is stuck on the ground";
+    }
+    
+    static function create() {
+      self::$instance_count++;
+      return new Bird;
+    }
+}
+
+class YellowBelliedFlyCatcher extends Bird {
+    var $name = "yellow-bellied flycatcher";
+    var $diet = "mostly insects.";
+    var $song = "flat chilk";
+    static $egg_num = "3-4, sometimes 5.";
+}
+
+class Kiwi extends Bird {
+    var $name = "kiwi";
+    var $diet = "omnivorous";
+    var $flying = "no";
+}
