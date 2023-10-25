7359117778a234a501c87256f553bb1916ab0d0d
Kira Beitler <majorwx2@gmail.com>
2023-10-24 20:49:19 -0400

    Fixing submodule issues

--- asgn07-oop-db-intro/private/db_credentials.php
+++ asgn07-oop-db-intro/private/db_credentials.php
@@ -6,8 +6,8 @@
 // 3. Unique credentials if working with multiple developers
 
 define("DB_SERVER", "localhost");
-define("DB_USER", "webuser");
-define("DB_PASS", "secretpassword");
-define("DB_NAME", "chain_gang");
+define("DB_USER", "ugmzsdbntput7");
+define("DB_PASS", "ijks7tdpvxjk");
+define("DB_NAME", "db6nuwjcpbtppu");
 
 ?>
