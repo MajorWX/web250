8a3e74401434f5bbbc6841acab8a345009de2be6
Kira Beitler <majorwx2@gmail.com>
2023-11-28 23:13:21 -0500

    Completed first section of asgn12

---
+++ asgn12-create-user/private/shared/public_header.php
@@ -0,0 +1,29 @@
+<!doctype html>
+
+<html lang="en">
+  <head>
+    <title>WNC Birds <?php if(isset($page_title)) { echo '- ' . h($page_title); } ?></title>
+    <meta charset="utf-8">
+  </head>
+
+  <body>
+
+    <header>
+      <h1>
+        <a href="<?php echo url_for('/index.php'); ?>">
+          WNC Birds
+        </a>
+      </h1>
+    </header>
+
+    <navigation>
+      <ul>
+        <?php if($session->is_logged_in()) { ?>
+        <li>User: <?php echo $session->username; ?></li>
+        <li><a href="<?php echo url_for('/index.php'); ?>">Menu</a></li>
+        <li><a href="<?php echo url_for('/logout.php'); ?>">Logout</a></li>
+        <?php } ?>
+      </ul>
+    </navigation>
+
+    <?php echo display_session_message(); ?>
